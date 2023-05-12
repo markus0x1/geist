df1ffac63abbe05cc7ff346dfeef0ae5ce026311
ümit.eth <47325422+UemitGuel@users.noreply.github.com>
2023-05-12 19:59:07 +0100

    add Xcode Project

---
+++ app/Geist/GeistApp.swift
@@ -0,0 +1,17 @@
+//
+//  GeistApp.swift
+//  Geist
+//
+//  Created by Ümit Gül on 12.05.23.
+//
+
+import SwiftUI
+
+@main
+struct GeistApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
