562e08cc190cf825b3aedd2c14c978a2e223d28b
KatharinaFranke <katharina.franke0x@gmail.com>
2023-05-12 21:12:41 +0100

    push it

---
+++ contracts/src/Token.sol
@@ -0,0 +1,15 @@
+// contracts/GLDToken.sol
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+
+// https://docs.openzeppelin.com/contracts/4.x/erc20
+
+contract GHO is ERC20 {
+    constructor(uint256 initialSupply) ERC20("GHO", "gho") {
+        _mint(msg.sender, initialSupply);
+    }
+}  
+// test token A to B (GHO)
