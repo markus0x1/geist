110d80f532b31a678ad08ca8b32f5e90e6e82d13
MarkuSchick <markuschick@gmail.com>
2023-05-13 14:31:35 +0100

    fix conflicts

---
+++ contracts/src/AppSpenderSigner.sol
@@ -0,0 +1,42 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import "./AppSpender.sol";
+
+/*
+        WALLET
+
+- to create a signature to send money we need
+    - amount
+    - resetTimeMin
+    - nonce
+    - apple id  
+    - token address
+    - sender contract
+    - chain id
+
+- to create a signature to claim money we need
+    - apple id
+    - token address
+    - receiver
+    - chain id
+*/
+
+
+abstract contract AppSpenderSigner is AppSpender {
+
+
+    function approveBySignature  (
+        uint256 amount,
+        uint16 resetTimeMin,
+        uint16 nonce,
+        uint256 id
+    ) public {
+        // UserOperation calldata userOp, bytes32 userOpHash
+        _approveId(amount, resetTimeMin, nonce, id);
+    }
+
+
+
+    function _validateSignature() internal virtual;
+}
