466d3601d50cbdf4bcac1ebd467808ff0b63a045
JD <jann.driessen@gmx.de>
2023-05-13 17:40:41 +0100

    Add GHO token

--- app/Geist/Constants.swift
+++ app/Geist/Constants.swift
@@ -13,3 +13,7 @@ enum Constants {
     static let privateKey0 = "0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80"
     static let privateKey1 = "0x59c6995e998f97a5a0044966f0945389dc9e86dae88c7a8412f4603b6b78690d"
 }
+
+enum Tokens {
+    static let GHO = "0x5FbDB2315678afecb367f032d93F642f64180aa3"
+}
