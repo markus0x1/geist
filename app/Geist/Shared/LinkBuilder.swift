eaf2423f891da3319bb5d467baa8c54d1e96e06d
JD <jann.driessen@gmx.de>
2023-05-13 12:26:35 +0100

    Add empty link builder

---
+++ app/Geist/Shared/LinkBuilder.swift
@@ -0,0 +1,15 @@
+//
+//  LinkBuilder.swift
+//  Geist
+//
+//  Created by Jann Driessen on 13.05.23.
+//
+
+import Foundation
+
+final class LinkBuilder {
+
+    func generateLink(for id: String) {}
+
+    func parseUrl(_ url: String) {}
+}
