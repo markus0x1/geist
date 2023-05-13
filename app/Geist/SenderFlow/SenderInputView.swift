f86e26eb4c0347ac6f29371661eba5c4ad2e095d
JD <jann.driessen@gmx.de>
2023-05-13 11:07:00 +0100

    Add empty sender start and input view

---
+++ app/Geist/SenderFlow/SenderInputView.swift
@@ -0,0 +1,20 @@
+//
+//  SenderInputView.swift
+//  Geist
+//
+//  Created by Jann Driessen on 13.05.23.
+//
+
+import SwiftUI
+
+struct SenderInputView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SenderInputView_Previews: PreviewProvider {
+    static var previews: some View {
+        SenderInputView()
+    }
+}
---
+++ app/Geist/SenderFlow/SenderStartView.swift
@@ -0,0 +1,20 @@
+//
+//  SenderStartView.swift
+//  Geist
+//
+//  Created by Jann Driessen on 13.05.23.
+//
+
+import SwiftUI
+
+struct SenderStartView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SenderStartView_Previews: PreviewProvider {
+    static var previews: some View {
+        SenderStartView()
+    }
+}
