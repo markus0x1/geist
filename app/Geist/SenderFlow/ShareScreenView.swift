34bea156f53776b3493b1f1743e6abab69167c10
JD <jann.driessen@gmx.de>
2023-05-13 19:23:05 +0100

    Update share screen style

--- app/Geist/SenderFlow/ShareScreenView.swift
+++ app/Geist/SenderFlow/ShareScreenView.swift
@@ -10,48 +10,52 @@ import Web3Core
 
 struct ShareScreenView: View {
     var url: URL = URL(string: "https://gho.xyz")!
+    private let backgroundColor: Color = GeistColor.purpleLight.opacity(0.2)
     @State private var showNextScreen: Bool = false
     var body: some View {
-        VStack {
-            Spacer()
-            Text(verbatim: "https://geist.xyz/claim?id=0x...")
-                .font(.system(size: 12))
-                .foregroundColor(GeistFontColor.secondaryTitle)
-                .multilineTextAlignment(.center)
-                .padding(.horizontal, 32)
-                .padding(.vertical, 16)
-                .overlay(
-                    RoundedRectangle(cornerRadius: 16)
-                        .stroke(GeistFontColor.borderSecondary, lineWidth: 1)
-                )
-                .padding(.vertical, 16)
-            Text("Link generated.")
-                .font(.system(size: 24))
-                .foregroundColor(GeistFontColor.secondaryTitle)
-                .multilineTextAlignment(.center)
-                .padding(.top, 16)
-            Text("Now send it to your fren.")
-                .font(.system(size: 24))
-                .foregroundColor(GeistFontColor.title)
-                .multilineTextAlignment(.center)
-                .padding(.bottom, 16)
-            Text("The person has 48h to claim it.")
-                .font(.system(size: 13))
-                .foregroundColor(GeistFontColor.secondaryTitle)
-                .multilineTextAlignment(.center)
-                .padding(.bottom, 16)
-            ShareLink(item: url, message: Text("Hey fren, I'm sending you \(Constants.amount) GHO! 👻")) {
-                Label("Share", systemImage:  "square.and.arrow.up")
+        ZStack {
+            backgroundColor.edgesIgnoringSafeArea(.all)
+            VStack {
+                Spacer()
+                Text("Link generated.")
+                    .font(.system(size: 24))
+                    .foregroundColor(GeistFontColor.secondaryTitle)
+                    .multilineTextAlignment(.center)
+                    .padding(.top, 16)
+                Text("Now send it to your fren.")
                     .font(.system(size: 24))
                     .foregroundColor(GeistFontColor.title)
+                    .multilineTextAlignment(.center)
+                    .padding(.bottom, 16)
+                Text(verbatim: "https://geist.xyz/claim?id=0x...")
+                    .font(.system(size: 12))
+                    .foregroundColor(GeistFontColor.secondaryTitle)
+                    .multilineTextAlignment(.center)
+                    .padding(.horizontal, 32)
+                    .padding(.vertical, 16)
+                    .overlay(
+                        RoundedRectangle(cornerRadius: 16)
+                            .stroke(GeistFontColor.borderSecondary, lineWidth: 1)
+                    )
+                    .padding(.vertical, 16)
+                Spacer()
+                ShareLink(item: url, message: Text("Hey fren, I'm sending you \(Constants.amount) GHO! 👻")) {
+                    Label("Share", systemImage:  "square.and.arrow.up")
+                        .font(.system(size: 24))
+                        .foregroundColor(GeistFontColor.title)
+                }
+                Text("The person has 48h to claim it.")
+                    .font(.system(size: 13))
+                    .foregroundColor(GeistFontColor.secondaryTitle)
+                    .multilineTextAlignment(.center)
+                    .padding(.vertical, 16)
+                Button("Share") {
+                    showNextScreen = true
+                }
+                .foregroundColor(backgroundColor)
             }
-            Spacer()
-            Button("Share") {
-                showNextScreen = true
-            }
-            .foregroundColor(.white)
+            .padding()
         }
-        .padding()
         .toolbar(.hidden)
         .navigationDestination(isPresented: $showNextScreen) {
             ShareSuccessView(amount: Constants.amount)
